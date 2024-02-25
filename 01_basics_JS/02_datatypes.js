9cba648e09d0ba5c3fef83ab1fc49ff184cc42b9
Ganeshbjamkar <ganesh.b.jamkar@gmail.com>
2024-02-25 18:17:09 +0530

    datatypes in js

---
+++ 01_basics_JS/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+
+/*Data tyepes :
+
+number => 2 to powe 53
+bigint
+string => ""
+boolean => true or false
+null => standalone value
+undefined => not assigned value
+symbol => unique
+object => 
+
+*/
+
+console.log((typeof ("gaanesh")));
+console.log((typeof (null)));
+console.log((typeof (undefined)));
