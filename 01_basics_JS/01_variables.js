9c16387ce73ddd46666c28062e4a30a554f3aed2
Ganeshbjamkar <ganesh.b.jamkar@gmail.com>
2024-02-25 17:49:53 +0530

    variables in js

---
+++ 01_basics_JS/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = "065210110006585"
+let accountMail = "ganesh@gmail.com"
+var accountPwd = " 1234"
+accountCity = "jaipur"
+
+accountMail = "gj@gmail.com"
+accountPwd = "3070"
+accountCity = "Loha"
+
+console.table([accountId, accountMail, accountPwd, accountCity])
+
+
+// prefer not to use var because block problem occures in old version of js.
