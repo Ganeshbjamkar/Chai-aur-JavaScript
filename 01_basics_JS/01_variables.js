3110c53e6d704475918503b318c95634ef6a9d2c
Ganeshbjamkar <ganesh.b.jamkar@gmail.com>
2024-04-03 10:25:42 +0530

    conversion of dataypes

--- 01_basics_JS/01_variables.js
+++ 01_basics_JS/01_variables.js
@@ -2,12 +2,13 @@ const accountId = "065210110006585"
 let accountMail = "ganesh@gmail.com"
 var accountPwd = " 1234"
 accountCity = "jaipur"
+let accountState;
 
 accountMail = "gj@gmail.com"
 accountPwd = "3070"
 accountCity = "Loha"
 
-console.table([accountId, accountMail, accountPwd, accountCity])
+console.table([accountId, accountMail, accountPwd, accountCity, accountState])
 
 
 // prefer not to use var because block problem occures in old version of js.
---
+++ 01_basics_JS/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = '33abc';
+let score1 = null
+let score2 = undefined
+
+console.log(typeof(score))
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(score1)
+console.log(score2)
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
