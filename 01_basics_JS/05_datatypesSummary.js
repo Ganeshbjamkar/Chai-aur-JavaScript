43cde459ef19c02f5eff18c850fe21ec52a718a4
Ganeshbjamkar <ganesh.b.jamkar@gmail.com>
2024-04-07 09:51:06 +0530

    prmitive and non-primitive datatypes

---
+++ 01_basics_JS/05_datatypesSummary.js
@@ -0,0 +1,33 @@
+// 1) Primitive datatype
+	
+// In JS 7 types of datatypes:
+
+// a. String
+// b. Number
+// c. Boolean
+// d. Null
+// e. Undefined
+// f. Symbol
+// g. BigInt
+
+// 2) Reference Datatype (Non Primitive datatype)
+
+// -- reference datatypes means directly which allocates the memory reference is called reference datatypes or non-primitive datatypes.
+
+// a. Array
+// b. Objects
+// c. Functions
+
+const heroes =  ["Shaktiman" , "Hatim", "Sonpari"]
+
+let myObj = {
+    name: "Ganesh",
+    age: 24,
+
+}
+
+const myFunction = function() {
+    console.log("Hello World")
+}
+
+
