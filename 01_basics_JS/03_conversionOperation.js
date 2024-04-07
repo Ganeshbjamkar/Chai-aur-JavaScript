4e86d6ddbe2df21ca6117b1522075a36282d0320
Ganeshbjamkar <ganesh.b.jamkar@gmail.com>
2024-04-07 09:23:07 +0530

    comparsion od opearotors in js

--- 01_basics_JS/03_conversionOperation.js
+++ 01_basics_JS/03_conversionOperation.js
@@ -1,19 +1,41 @@
-let score = '33abc';
-let score1 = null
-let score2 = undefined
+// let score = '33abc';
+// let score1 = null
+// let score2 = undefined
 
-console.log(typeof(score))
-console.log(typeof score)
+// console.log(typeof(score))
+// console.log(typeof score)
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
-console.log(score1)
-console.log(score2)
+// console.log(typeof valueInNumber)
+// console.log(score1)
+// console.log(score2)
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+// --------------------------------------------Operations-----------------------------------
+
+let value = 1;
+let negativeValue = -value
+console.log(negativeValue)
+
+console.log(2/3)
+console.log(2%3)
+console.log(1 + "2")
+console.log("1" + 2)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(true)
+console.log(+true)
+console.log(-true)
+
+console.log(false)
+console.log(+false)
+console.log(-false)
 
 
---
+++ 01_basics_JS/04_comparisonOperator.js
@@ -0,0 +1,16 @@
+// these operator just try to understand but dont use tghem for clean code.
+
+
+console.log("2" > 1)
+
+console.log("02" > 1)
+
+console.log(null > 0)
+
+console.log(null == 0)
+
+console.log(null >= 0)
+
+console.log(first)
+
+
